5531c24296a238c6afa3759c6c814d665ab2a9b3
Edward Kwan <edward.kwan@education.nsw.gov.au>
2025-06-27 00:56:34 +1000

    w9 thur

--- class Map:.py.py
+++ class Map:.py.py
@@ -1,11 +1,19 @@
 import random
 
-
+#for multi dimensional  array
 w, h = 8, 8 # width and height
 grid = [['-' for _ in range(w)] for _ in range(h)] # all grid will by -
 x, y = 4, 4  # player start coordinate
+grid [y][x] = '0'
+def showgrid():
+ for show in grid:
+     print(show)
+
+    
+showgrid() # whean start show grid
 
 while True:
+    
 
     move = input("WASD (L to leave): ").lower()
     # i search google of -=, += 
@@ -20,3 +28,8 @@ while True:
     elif move == 'l': break
     else:
          ('type correctly')
+    
+
+
+    grid[y][x] = '0' # update the position it by using -=, +- etc
+    showgrid() # update print grid
