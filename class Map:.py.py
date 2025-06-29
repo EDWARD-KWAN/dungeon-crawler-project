f1ab6bcd13b50a15860ca380113c94f808fc054e
Edward Kwan <edward.kwan@education.nsw.gov.au>
2025-06-29 14:08:25 +1000

    map code look ready

--- class Map:.py.py
+++ class Map:.py.py
@@ -1,35 +1,54 @@
 import random
 
-#for multi dimensional  array
-w, h = 8, 8 # width and height
-grid = [['-' for _ in range(w)] for _ in range(h)] # all grid will by -
-x, y = 4, 4  # player start coordinate
-grid [y][x] = '0'
-def showgrid():
- for show in grid:
-     print(show)
+def map(): # put define function
+    w, h = 8, 8  # width and height to put 8x8 grid on 2d way
+    grid = [['-' for _ in range(w)] for _ in range(h)]  # initialize grid
+    x, y = 0, 0  # realise they use zero based indexing no wonder i was confused
+    grid[y][x] = '0'  # mark player
 
-    
-showgrid() # whean start show grid
+    def showgrid():
+        for row in grid:
+            print(' '.join(row)) #remove the bracket
+        print()
+        pass
+  
+    def findsomething():
+        find = random.choice(['nothing', 'enemy', 'heal'])
+        if find == 'enemy':
+            print('enemy')
+            pass
+        elif find == 'heal':
+            print('heal')
+        elif find == 'nothing':
+            print ( 'nothin')
+        
 
-while True:
-    
+    showgrid() # print it
 
-    move = input("WASD (L to leave): ").lower()
-    # i search google of -=, += 
-    if move == 'w':
-         y -= 1 # opposite
-    elif move == 's':
-         y += 1 # down because higher it get it goes down in height
-    elif move == 'a':
-         x -= 1 # opposite
-    elif move == 'd':
-         x += 1 # if this it goes ->  since it move by adding 1 in width
-    elif move == 'l': break
-    else:
-         ('type correctly')
-    
+    while True:
+        move = input("Move (WASD) or L to leave: ").lower()
 
+        if move == 'l':
+            print("Leaving the map...")
+            break # use for save implatation
 
-    grid[y][x] = '0' # update the position it by using -=, +- etc
-    showgrid() # update print grid
+        # remove the used player pos old position when it loop again
+        grid[y][x] = '-'
+
+        if move == 'w' and y > 0: # if y greater/higher than 0 it would go up vertical in a 2d dimention when connecting with it
+            y -= 1 # it minus the grid section so it will be going up since
+        elif move == 's' and y < h - 1: # due to zero based index need -1  so h need to be 7 
+            y += 1                      # mean y < 7
+        elif move == 'a' and x > 0: #opposite of  y > 0
+            x -= 1
+        elif move == 'd' and x < w - 1: # x < 7
+            x += 1
+        else:
+            print("Invalid move or edge of map!")
+        
+        # update new position
+        grid[y][x] = '0'
+        showgrid()
+        findsomething()
+
+map()
