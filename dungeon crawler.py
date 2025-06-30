cd67f06483a9ac6f9d0d23935baf12e22727ad05
Edward Kwan <edward.kwan@education.nsw.gov.au>
2025-06-30 22:08:51 +1000

    start file

--- dungeon crawler.py
+++ dungeon crawler.py
@@ -96,16 +96,84 @@ def combat():
      time.sleep(0) 
     if not player_set.alive():
      print("You have Varnish")
+     menu()
      return
     
    if not opponent.alive() == True:
      print(f'You defeat {opponent.name} \n continue your journey')
+     map()
 
+def map(): # put define function
+    w, h = 8, 8  # width and height to put 8x8 grid on 2d way
+    grid = [['-' for _ in range(w)] for _ in range(h)]  # initialize grid
+    x, y = 5, 5  # realise they use zero based indexing no wonder i was confused
+    grid[y][x] = '0'  # mark player
+
+    def showgrid():
+        for row in grid:
+            print(' '.join(row)) #remove the bracket
+        print()
+        pass
+  
+    def findsomething():
+        find = random.choice(['nothing', 'enemy', 'heal'])
+        if find == 'enemy':
+            combat()
+        elif find == 'heal':
+            print('u find a camp in your endless journey u increase your health ')
+        elif find == 'nothing':
+            print ( 'your continue walking day to day in wat search within it')
+        
+
+    showgrid() # print it
+
+    while True:
+        move = input("Move (WASD) or L to leave: ").lower()
+
+        if move == 'l':
+            print("Leaving the map...")
+            break # use for save implatation
+
+        # remove the used player pos old position when it loop again
+        grid[y][x] = '-'
+
+        if move == 'w' and y > 0: # if y greater/higher than 0 it would go up vertical in a 2d dimention when connecting with it
+            y -= 1 # it minus the grid section so it will be going up since
+        elif move == 's' and y < h - 1: # due to zero based index need -1  so h need to be 7 
+            y += 1                      # mean y < 7
+        elif move == 'a' and x > 0: #opposite of  y > 0
+            x -= 1
+        elif move == 'd' and x < w - 1: # x < 7
+            x += 1
+        else:
+            print("Invalid move or edge of map!")
+        
+        # update new position
+        grid[y][x] = '0'
+        showgrid()
+        findsomething()
 
 
   
-  
+def save_game():
+ with open ("save_game.txt", "w") as f:
+   f.write(f"{player_set.name}\n")
+   f.write(f"{player_set.health}\n")
+   f.write(f"{player_set.damage}\n")
+   f.write(f"{player_set.equipment.name}\n")
+   f.write(f"{player_set.score}\n")
+   print("this file is save")
 
+def all_previous_highscore():
+ with open ("highscore.txt", "w") as f:
+   f.write(f"{player_set.name}\n")
+   f.write(f"{player_set.score}\n")
+   print('ur high score')
+
+def load_game():
+ global player_set
+ with open("save_game.txt", "r") as f:
+  pass
 
  
  
@@ -164,7 +232,7 @@ def continue_play ():
   return # dosen't continue to start combat(). will start error
 
  print (f'hello {player_set.name} your journey have process')
- combat()
+ map()
 
 player_set =None # found that using none as null value is good so it won't give me error to define
 
